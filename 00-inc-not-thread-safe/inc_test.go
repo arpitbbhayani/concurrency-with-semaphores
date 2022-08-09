572603a84f153132f667124b42fd8823f77ae21b
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2022-08-09 21:39:21 +0530

    adding a quick test for 00

--- 00-inc-not-thread-safe/inc_test.go
+++ 00-inc-not-thread-safe/inc_test.go
@@ -18,17 +18,25 @@ var scenarios = []struct {
 	{numThread: 1000},
 }
 
+func Test(t *testing.T) {
+	reset()
+	expectedCount, observedCount := parallelInc(1000)
+	if expectedCount != observedCount {
+		t.Errorf("observed count %d did not match the expected count %d", observedCount, expectedCount)
+	}
+}
+
 func BenchmarkInc(b *testing.B) {
 	for _, v := range scenarios {
 		b.Run(fmt.Sprintf("numThread-%d", v.numThread), func(b *testing.B) {
 			countMatched := 0
 			for i := 0; i < b.N; i++ {
+				reset()
 				expectedCount, observedCount := parallelInc(v.numThread)
 				if expectedCount == observedCount {
 					countMatched++
 					b.Setenv("MATCHED_COUNT", strconv.Itoa(countMatched))
 				}
-				reset()
 			}
 			b.Logf("expected count matched the observed count %s out of %d times when number of threads were %d", os.Getenv("MATCHED_COUNT"), b.N, v.numThread)
 		})
