a024876ec4f2c92d05ab293c7d9c08dea2484918
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2022-08-10 08:24:28 +0530

    c implementation

---
+++ 00-inc-not-thread-safe.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define THREAD_COUNT 1000
+
+int count = 0;
+
+void * inc_count() {
+  for (int i = 0; i < THREAD_COUNT; i++) {
+    count ++;
+  }
+  return NULL;
+}
+
+void reset() {
+  count = 0;
+}
+
+void test_correctness() {
+  pthread_t th[THREAD_COUNT];
+
+  reset();
+
+  for (int i = 0; i < THREAD_COUNT; i++) {
+    if (pthread_create(&th[i], NULL, &inc_count, NULL) != 0) {
+      perror("failed to create thread");
+    }
+  }
+
+  for (int i = 0; i < THREAD_COUNT; i++) {
+    if (pthread_join(th[i], NULL) != 0) {
+      perror("failed to join thread");
+    }
+  }
+
+  printf("expected %d, observed %d\n", THREAD_COUNT * THREAD_COUNT, count);
+}
+
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < 10; i++) {
+    test_correctness();
+  }
+  return 0;
+}
