913207dd7576c6715ab32d0223dae2b0c3ab4d6a
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2022-08-14 23:19:19 +0530

    unhappy royal chef comments

--- 01-royal-chef-unhappy.c
+++ 01-royal-chef-unhappy.c
@@ -5,6 +5,7 @@
 
 #define THREAD_COUNT 1000
 
+// Activities of the Chef and the King
 typedef enum { 
   CHEF_WAKES_UP,
   CHEF_PREPARATION_START,
@@ -17,12 +18,16 @@ typedef enum {
   KING_ANGRY,
 } activity_t;
 
+
+// represents if the lunch is ready or not
 int is_prepared = 0;
 
+// random delay while doing an activity
 void act(activity_t activity) {
   usleep(50);
 }
 
+// chef's routine
 void * routine_chef() {
   act(CHEF_WAKES_UP);
   act(CHEF_PREPARATION_START);
@@ -32,20 +37,28 @@ void * routine_chef() {
   return NULL;
 }
 
+
+// king's routine
 void * routine_king() {
   act(KING_WAKES_UP);
   act(KING_ADDRESSES_PUBLIC);
   act(KING_VISITS_DINING_AREA);
 
+  // if king sees that the food is not prepared
+  // after visiting the dining area
+  // the king gets angry
   if (!is_prepared) {
     act(KING_ANGRY);
     return (void *) KING_ANGRY;
   }
   
+  // otherwise the king eats the lunch
   act(KING_EATS_LUNCH);
   return (void *) KING_EATS_LUNCH;
 }
 
+
+// resets to base 0
 void reset() {
   is_prepared = 0;
   srand(time(NULL));
@@ -56,23 +69,29 @@ void test() {
 
   reset();
 
+  // start the chef's routine
   if (pthread_create(&th_chef, NULL, &routine_chef, NULL) != 0) {
     perror("failed to create the chef thread");
   }
 
+  // start the king's routine
   if (pthread_create(&th_king, NULL, &routine_king, NULL) != 0) {
     perror("failed to create the king thread");
   }
 
+  // wait for the threads to complete
   if (pthread_join(th_chef, NULL) != 0) {
     perror("failed to join on chef thread");
   }
 
+  // check the return value of king's routine
+  // and see if he was angry or not
   activity_t state_king;
   if (pthread_join(th_king, (void *) &state_king) != 0) {
     perror("failed to join on king thread");
   }
 
+  // print if king was angry
   printf("is king angry: %d\n", state_king == KING_ANGRY);
 }
 
